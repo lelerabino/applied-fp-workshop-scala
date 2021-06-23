16c36c86bcb4736601e0f88b7f1c58700409f83b
Emanuele Rabino <e.rabino@teamsystem.com>
2021-06-23 12:17:20 +0200

    (day1): model behaviours

--- exercises/src/test/scala/firststeps/ModelBehaviours.scala
+++ exercises/src/test/scala/firststeps/ModelBehaviours.scala
@@ -23,11 +23,23 @@ class ModelBehaviours extends munit.FunSuite {
   val reciprocal: Int => Double = in => 1.0 / in
 
   val reciprocalString: String => String = in => {
-    ???
+    asString(reciprocal(parseString(in)))
   }
 
-  test("from string to string throught reciprocal".ignore) {
-    // TODO: use existing function to compute a reciprocal in string
+  val numericReciprocalOfString: String => Double = in =>  reciprocal(parseString(in))
+
+  val reciprocalString1: String => String = in => {
+    (asString compose numericReciprocalOfString)(in)
+  }
+
+
+
+
+  test("from string to string throught reciprocal") {
     assertEquals(reciprocalString("42"), "0.023809523809523808")
+  }
+
+  test("from string to string throught reciprocal1") {
+    assertEquals(reciprocalString1("42"), "0.023809523809523808")
   }
 }
